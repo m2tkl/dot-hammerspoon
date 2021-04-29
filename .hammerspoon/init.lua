b1183b7415288491b312cf068822b9cded70a26d
m2tkl <kenchalros@gmail.com>
2021-04-29 18:35:05 +0900

    1/4サイズの調整を追加

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -7,15 +7,23 @@ units = {
   left50        = { x = 0.00, y = 0.00, w = 0.50, h = 1.00 },
   top50         = { x = 0.00, y = 0.00, w = 1.00, h = 0.50 },
   bot50         = { x = 0.00, y = 0.50, w = 1.00, h = 0.50 },
-  maximum       = { x = 0.00, y = 0.00, w = 1.00, h = 1.00 }
+  maximum       = { x = 0.00, y = 0.00, w = 1.00, h = 1.00 },
+  topleft       = { x = 0.00, y = 0.00, w = 0.50, h = 0.50 },
+  topright      = { x = 0.50, y = 0.00, w = 0.50, h = 0.50 },
+  botleft    = { x = 0.00, y = 0.50, w = 0.50, h = 0.50 },
+  botright   = { x = 0.50, y = 0.50, w = 0.50, h = 0.50 }
 }
 
 mash = {'option', 'ctrl', 'cmd'}
 hs.hotkey.bind(mash, 'right', function() hs.window.focusedWindow():move(units.right50, nil, true) end)
-hs.hotkey.bind(mash, 'left', function() hs.window.focusedWindow():move(units.left50, nil, true) end)
-hs.hotkey.bind(mash, 'up', function() hs.window.focusedWindow():move(units.top50, nil, true) end)
-hs.hotkey.bind(mash, 'down', function() hs.window.focusedWindow():move(units.bot50, nil, true) end)
-hs.hotkey.bind(mash, 'm', function() hs.window.focusedWindow():move(units.maximum, nil, true) end)
+hs.hotkey.bind(mash, 'left',  function() hs.window.focusedWindow():move(units.left50, nil, true) end)
+hs.hotkey.bind(mash, 'up',    function() hs.window.focusedWindow():move(units.top50, nil, true) end)
+hs.hotkey.bind(mash, 'down',  function() hs.window.focusedWindow():move(units.bot50, nil, true) end)
+hs.hotkey.bind(mash, 'm',     function() hs.window.focusedWindow():move(units.maximum, nil, true) end)
+hs.hotkey.bind(mash, '1',     function() hs.window.focusedWindow():move(units.topleft, nil, true) end)
+hs.hotkey.bind(mash, '2',     function() hs.window.focusedWindow():move(units.topright, nil, true) end)
+hs.hotkey.bind(mash, '3',     function() hs.window.focusedWindow():move(units.botleft, nil, true) end)
+hs.hotkey.bind(mash, '4',     function() hs.window.focusedWindow():move(units.botright, nil, true) end)
 
 
 --
